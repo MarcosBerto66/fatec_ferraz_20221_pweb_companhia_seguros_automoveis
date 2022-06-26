dbf72c02b00701775a7e0d80b0c1323006b494b0
Marcos Berto <marcosberto66@gmail.com>
2022-06-25 23:47:07 -0300

    Atualização dos arquivos de banco de dados.

--- db_arquivos/db_script_companhia_seguros_automoveis.sql
+++ db_arquivos/db_script_companhia_seguros_automoveis.sql
@@ -1,54 +1,47 @@
 /* db_modelo_logico_companhia_seguros_automoveis: */
 
+CREATE DATABASE db_companhia_seguros_automoveis;
+GO
+USE db_companhia_seguros_automoveis
+
 CREATE TABLE tbCliente (
-    idCliente INTEGER PRIMARY KEY,
-    nomeCliente VARCHAR,
-    rgCliente CHARACTER,
-    cpfCliente CHARACTER,
-    ufCliente CHARACTER,
-    cidadeCliente VARCHAR,
-    logradouroCliente VARCHAR,
-    bairroCliente VARCHAR,
-    cepCliente CHARACTER,
-    numCliente VARCHAR
+    idCliente INT PRIMARY KEY,
+    nomeCliente VARCHAR(200) NOT NULL,
+    rgCliente  VARCHAR(12) NOT NULL,
+    cpfCliente VARCHAR(14) NOT NULL,
+    ufCliente CHAR(2) NOT NULL,
+    cidadeCliente VARCHAR(200) NOT NULL,
+    logradouroCliente VARCHAR(200) NOT NULL,
+    bairroCliente VARCHAR(200) NOT NULL, 
+    cepCliente CHAR(9) NOT NULL,
+    numCliente VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE tbVeiculo (
-    idVeiculo INTEGER PRIMARY KEY,
-    placaVeiculo CHARACTER,
-    renavamVeiculo CHARACTER,
-    anoVeiculo SMALLINT,
-    fabricanteVeiculo VARCHAR,
-    idCliente INTEGER
+    idVeiculo INT PRIMARY KEY,
+    placaVeiculo CHAR(7) NOT NULL,
+    renavamVeiculo CHAR(12) NOT NULL,
+    anoVeiculo SMALLINT NOT NULL,
+    fabricanteVeiculo VARCHAR(200) NOT NULL,
+    idCliente INT NOT NULL,
+	FOREIGN KEY (idCliente) REFERENCES tbCliente(idCliente)
 );
 
 CREATE TABLE tbOcorrencia (
-    idOcorrencia INTEGER PRIMARY KEY,
-    dataOcorrencia DATE,
-    cidadeOcorrencia VARCHAR,
-    logradouroOcorrencia VARCHAR,
-    bairroOcorrencia VARCHAR,
-    cepOcorrencia CHARACTER,
-    ufOcorrencia CHARACTER,
-    idVeiculo INTEGER
+    idOcorrencia INT PRIMARY KEY,
+    dataOcorrencia DATE NOT NULL,
+    cidadeOcorrencia VARCHAR(200) NOT NULL,
+    logradouroOcorrencia VARCHAR(200) NOT NULL,
+    bairroOcorrencia VARCHAR(200) NOT NULL,
+    cepOcorrencia CHAR(9) NOT NULL,
+    ufOcorrencia CHAR(2) NOT NULL,
+    idVeiculo INT NOT NULL,
+	FOREIGN KEY (idVeiculo) REFERENCES tbVeiculo(idVeiculo)
 );
 
 CREATE TABLE tbTelefoneCliente (
-    idTelefoneCliente INTEGER PRIMARY KEY,
-    numTelefoneCliente VARCHAR,
-    idCliente INTEGER
+    idTelefoneCliente INT PRIMARY KEY,
+    numTelefoneCliente VARCHAR(14) NOT NULL,
+    idCliente INT NOT NULL,
+	FOREIGN KEY (idCliente) REFERENCES tbCliente(idCliente)
 );
- 
-ALTER TABLE tbVeiculo ADD CONSTRAINT FK_tbVeiculo_2
-    FOREIGN KEY (idCliente)
-    REFERENCES tbCliente (idCliente)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE tbOcorrencia ADD CONSTRAINT FK_tbOcorrencia_2
-    FOREIGN KEY (idVeiculo)
-    REFERENCES tbVeiculo (idVeiculo)
-    ON DELETE CASCADE;
- 
-ALTER TABLE tbTelefoneCliente ADD CONSTRAINT FK_tbTelefoneCliente_2
-    FOREIGN KEY (idCliente???)
-    REFERENCES tbCliente (???);
